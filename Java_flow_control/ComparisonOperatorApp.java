ff75226560d5d195955a9ba9577d3be78a1332cc
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-01 21:42:10 +0900

    비교 연산자

---
+++ Java_flow_control/ComparisonOperatorApp.java
@@ -0,0 +1,17 @@
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+
+		System.out.println(1 + 1);	// +는 숫자와 숫자를 더하는 산술 연산자
+		System.out.println("1" + "1");	// +는 문자와 문자를 결합해주는 결합 연산자
+		
+		// 비교 연산자
+		System.out.println(1 > 1);	// false
+		System.out.println(1 == 1);	// true
+		System.out.println(1 < 1);	// false
+		System.out.println(1 >= 1);	// true
+		
+	}
+
+}
